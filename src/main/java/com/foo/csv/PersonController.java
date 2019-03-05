afe3360512d139ca55fce5106ae84901c0a77b33
cheekychinnu <vinodhinic@gmail.com>
2019-03-06 00:44:10 +0530

    I have a use case where I stich the request object with the corresponding error message. I don't see any direct example which merges two schema -one of the request and another of response and a simple write(req, res) should give the csv.
    So here I am converting request to JSON and then adding response to this Json object. collecting all such json object in a Json array and using various other libraries available to get the csv string and directly writing to the response writer.
    
    There is no solution to preserve the order of headers. But this will do for my usecase

--- src/main/java/com/foo/csv/PersonController.java
+++ src/main/java/com/foo/csv/PersonController.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsontocsv.parser.JSONFlattener;
+import org.jsontocsv.writer.CSVWriter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -46,6 +50,43 @@ public class PersonController {
         Person p2 = new Person("Dagney Taggart", 38, "NewYork");
         List<Person> inputs = Stream.of(p1, p2).collect(Collectors.toList());
         write(Person.class, inputs, response);
+    }
+
+    @RequestMapping(value = "/downloadWithErrorMessage", method = RequestMethod.GET)
+    public void downloadCSVWithStitchedInformation(HttpServletResponse response) throws Exception {
+        Person p1 = new Person("Hank Reardon", 47, "California");
+        p1.setCity(null);
+        Person p2 = new Person("Dagney Taggart", 38, "NewYork");
+        p2.setAge(null);
+
+        JSONArray ja = new JSONArray();
+        List<Person> inputs = Stream.of(p1, p2).collect(Collectors.toList());
+        for (Person p : inputs) {
+            JSONObject jsonObject = new JSONObject(p);
+            jsonObject.put("errorMessage", "something");
+            ja.put(jsonObject);
+        }
+
+        /*
+        This one does not create header by reading all object.
+        String csv = CDL.toString(ja);
+        System.out.print(csv);
+        */
+        /*
+        This one also works. but does not have utility to read just the csv string
+        String jsonString = ja.toString();
+
+        JFlat jflat = new JFlat(jsonString);
+        jflat.json2Sheet().headerSeparator(",").write2csv("something.csv");
+        */
+
+        String csv = CSVWriter.getCSV(JSONFlattener.parse(ja), ",");
+        System.out.print(csv);
+        response.setContentType("application/force-download");
+        response.setHeader("Content-Disposition", "attachment; filename=" + "sample.csv");
+        response.getWriter().print(csv);
+        response.flushBuffer();
+//        write(Person.class, inputs, response);
     }
 
     // Example when you need to download from resource
